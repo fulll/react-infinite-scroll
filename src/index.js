6a81efc33390386bf3013eebf46f77506d468176
igor13127 <igor.eschalier@gmail.com>
2016-09-21 10:57:12 +0200

    Guidelines fixes

--- src/index.js
+++ src/index.js
@@ -1,75 +1,63 @@
 import React from 'react'
 
 const InfiniteScrollSpinner = () => {
+  const style = {
+    width: '100%',
+    fontSize: 12,
+    textAlign: 'center',
+    padding: '0 0 10px 0',
+  }
 
-    let style = {
-      width: '100%',
-      fontSize: 12,
-      textAlign: 'center',
-      padding: '0 0 10px 0'
-    }
-
-    return <div style={style}>Loading ...</div>
+  return <div style={style}>Loading ...</div>
 }
 
 const InfiniteScrollReloader = () => {
+  const style = {
+    width: '100%',
+    fontSize: 12,
+    textAlign: 'center',
+    cursor: 'pointer',
+    padding: '10px 0',
+  }
 
-    let style = {
-      width: '100%',
-      fontSize: 12,
-      textAlign: 'center',
-      cursor: 'pointer',
-      padding: '10px 0'
-    }
-
-    return (
-      <div style={style}>
-        Error loading data, click to try again
-      </div>
-    )
+  return (
+    <div style={style}>
+      Error loading data, click to try again
+    </div>
+  )
 }
 
 const InfiniteScroll = (props) => {
+  let { height, threshold } = props
 
-  let {
-    height,
-    items,
-    loadMore,
-    hasMore,
-    loading,
-    error,
-    spinner,
-    reloader,
-    threshold,
-  } = props
+  const { items, loadMore, loading, spinner, error, hasMore, reloader } = props
 
-  height = height ? height : '100%'
-  threshold = threshold ? threshold : 0
+  height = height || '100%'
+  threshold = threshold || 0
 
   const style = {
     div: {
       overflowX: 'hidden',
       overflowY: 'auto',
       WebkitOverflowScrolling: 'touch',
-      height
-    }
+      height,
+    },
   }
 
-  let customReloader = reloader ? reloader : <InfiniteScrollReloader />
+  const customReloader = reloader || <InfiniteScrollReloader />
 
-  let customSpinner = spinner ? spinner : <InfiniteScrollSpinner />
-  let displaySpinner = hasMore ? true : false
+  const customSpinner = spinner || <InfiniteScrollSpinner />
+  const displaySpinner = hasMore
 
   const loadMoreElements = (e) => {
-
-    let componentHeight = e.target.scrollHeight - threshold - 1
-    let currentPosition = e.target.offsetHeight + e.target.scrollTop
+    const componentHeight = e.target.scrollHeight - threshold - 1
+    const currentPosition = e.target.offsetHeight + e.target.scrollTop
 
     if (currentPosition >= componentHeight
-      && hasMore == true
-      && loading == false
-      && error == false) {
-        loadMore()
+      && hasMore === true
+      && loading === false
+      && error === false) {
+      loadMore()
     }
   }
 
@@ -77,15 +65,15 @@ const InfiniteScroll = (props) => {
     loadMore()
   }
 
+  const showSpinner = displaySpinner ? customSpinner : null
+
   return (
     <div style={style.div} onScroll={loadMoreElements}>
-        {items}
-        {error ?
-          <div onClick={tryAgain}>{customReloader}</div>
-          : displaySpinner ?
-            customSpinner
-            : null
-        }
+      {items}
+      {error ?
+        <div onClick={tryAgain}>{customReloader}</div>
+        : showSpinner
+      }
     </div>
   )
 }
